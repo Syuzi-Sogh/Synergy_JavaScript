5b35209b5a31575bb71503d62456d2d77e1435f2
Syuzi <sogomonansuzi@gmail.com>
2025-04-11 23:10:18 +0500

    first

---
+++ 1.1.js
@@ -0,0 +1,10 @@
+console.log("Домашняя работа по итогам урока 1");
+let name = "Ann"
+let age = 21
+let marital_status = false
+
+console.log("Имя: " + name)
+console.log("Возраст: " + age)
+console.log("Есть супруг/супруга: " + marital_status)
+
+console.log("Тип данный переменных ")
---
+++ 1.js
@@ -0,0 +1,11 @@
+/*
+Типы данный в javascript: number, brigint,string, boolean, null, undefined, object, symbol
+typeof() - возвращает тип
+condole.log("Smth" + n);
+
+let s2 = 'world'
+let s3 = `Hello, ${str2}`; - выводит hello, world
+вместо str2 может быть число, выражение или т.д.
+null =  для пустого
+underfined = были ли переменная назрначена?
+ */
